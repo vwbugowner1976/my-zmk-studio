39e7d6d2c9edd161dda73b8d9ccff56e0da037a3
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-29 09:23:22 +0900

    Load layer viewer styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import DebugConsole from './DebugConsole';
 import './styles.css';
+import './layerViewer.css';
 import './debugConsoleOverride.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
