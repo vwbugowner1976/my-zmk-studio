0d3fa80c3fc990a47ecc5c845697f907e999a30c
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-29 19:36:42 +0900

    feat: show keyboard name and prefix export filenames

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import DebugConsole from './DebugConsole';
+import DeviceNameBadge from './DeviceNameBadge';
+import { installDeviceExportNaming } from './deviceIdentity';
 import './styles.css';
 import './layerViewer.css';
 import './bindingPicker.css';
@@ -9,10 +11,14 @@ import './comboEditor.css';
 import './v05.css';
 import './keymapDiffGuide.css';
 import './debugConsoleOverride.css';
+import './deviceName.css';
+
+installDeviceExportNaming();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DeviceNameBadge />
     <DebugConsole />
   </React.StrictMode>,
 );
