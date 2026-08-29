2f7b4f2ccf37aecd3942c80db3eba3090f7db841
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-29 09:54:48 +0900

    Load v0.5 menu and backup styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import DebugConsole from './DebugConsole';
 import './styles.css';
 import './layerViewer.css';
+import './v05.css';
 import './debugConsoleOverride.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
