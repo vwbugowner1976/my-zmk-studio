33cf5401489334bce5a76f1326e275c79e960a4c
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-29 09:15:55 +0900

    Show only persistent debug console

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import DebugConsole from './DebugConsole';
 import './styles.css';
+import './debugConsoleOverride.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
