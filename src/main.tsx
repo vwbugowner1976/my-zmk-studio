bd605212ade4e38a0c8beebf7a99713ac52f58d8
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-29 09:13:42 +0900

    feat: keep debug console visible after disconnect

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DebugConsole from './DebugConsole';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DebugConsole />
   </React.StrictMode>,
 );
