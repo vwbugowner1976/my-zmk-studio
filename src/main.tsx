21a5e5b207041bf1ebbcd5c3a19311113ea64bee
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-08-30 09:30:08 +0900

    feat: mount sensor bindings readiness panel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import DebugConsole from './DebugConsole';
 import DeviceNameBadge from './DeviceNameBadge';
+import SensorBindingsPortal from './SensorBindingsPanel';
 import { installDeviceExportNaming } from './deviceIdentity';
 import './styles.css';
 import './layerViewer.css';
+import './sensorBindings.css';
 import './bindingPicker.css';
 import './comboEditor.css';
 import './customSettings.css';
@@ -19,6 +21,7 @@ installDeviceExportNaming();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SensorBindingsPortal />
     <DeviceNameBadge />
     <DebugConsole />
   </React.StrictMode>,
